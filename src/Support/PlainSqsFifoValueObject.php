ae2fd98f4a1c28ab48b6659efa0f0cb844f2e007
Jesus Ortega <jesus.ortega@housfy.com>
2022-10-17 15:27:12 +0200

    Remove setter and update readme

--- src/Support/PlainSqsFifoValueObject.php
+++ src/Support/PlainSqsFifoValueObject.php
@@ -47,23 +47,9 @@ class PlainSqsFifoValueObject
         return $this->id;
     }
 
-    public function setId(string $id): self
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
     public function type(): string
     {
         return $this->type;
-    }
-
-    public function setType(string $type): self
-    {
-        $this->type = $type;
-
-        return $this;
     }
 
     public function occurredOn(): ?string
@@ -71,22 +57,9 @@ class PlainSqsFifoValueObject
         return $this->occurred_on;
     }
 
-    public function setOccurredOn(string $occurred_on): self
-    {
-        $this->ocurred_on = $occurred_on;
-        return $this;
-    }
-
     public function attributes(): array
     {
         return $this->attributes;
-    }
-
-    public function setAttributes(array $attributes): self
-    {
-        $this->attributes = $attributes;
-
-        return $this;
     }
 
     public function meta(): array
@@ -94,31 +67,13 @@ class PlainSqsFifoValueObject
         return $this->meta;
     }
 
-    public function setMeta(array $meta): self
-    {
-        $this->meta = $meta;
-        return $this;
-    }
-
     public function group()
     {
         return $this->group;
     }
 
-    public function setGroup(string $group): self
-    {
-        $this->group = $group;
-        return $this;
-    }
-
     public function connection()
     {
         return $this->connection;
-    }
-
-    public function setConnection(string $connection)
-    {
-        $this->connection = $connection;
-        return $this;
     }
 }
